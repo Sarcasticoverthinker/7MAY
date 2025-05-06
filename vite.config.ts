549dc0e95efefd25ae7eca40aecb0f7fedc1afaf
Yasir <39470795+Sarcasticoverthinker@users.noreply.github.com>
2025-05-07 03:05:01 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react', 'react-confetti', 'react-sound'],
-  },
   build: {
     rollupOptions: {
-      external: ['react-confetti', 'react-sound'], // Externalize these libraries if needed
+      external: ['react-confetti', 'react-sound'],
+      output: {
+        globals: {
+          'react-confetti': 'Confetti',
+          'react-sound': 'ReactSound',
+        },
+      },
     },
   },
-});
+}); 
