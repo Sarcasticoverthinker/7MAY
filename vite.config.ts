6c76e621966d982dadf0f3c97f1a2621180adb0b
Yasir <39470795+Sarcasticoverthinker@users.noreply.github.com>
2025-05-07 03:48:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sarcasticoverthinker.github.io/',  // Add the repo name here
+  base: '/sarcasticoverthinker.github.io/',  // Add the base path for GitHub Pages
   plugins: [react()],
   build: {
-    outDir: 'dist',
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name]-[hash][extname]',
