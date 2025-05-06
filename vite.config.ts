062d7c737980e57e16bd1466a697ecc05371ddc6
Yasir <39470795+Sarcasticoverthinker@users.noreply.github.com>
2025-05-07 02:39:41 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Sarcasticoverthinker.github.io/', // <- Replace this with your actual GitHub repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+ 
