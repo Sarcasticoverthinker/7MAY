89a736f412f23a3811db32c95c6ee5b4564214b1
Yasir <39470795+Sarcasticoverthinker@users.noreply.github.com>
2025-05-07 03:12:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/sarcasticoverthinker.github.io/', // Replace with your GitHub repository name
   build: {
+    outDir: 'dist', // Optional: You can change the output directory if needed
     rollupOptions: {
-      external: ['react-confetti', 'react-sound'],
-      output: {
-        globals: {
-          'react-confetti': 'Confetti',
-          'react-sound': 'ReactSound',
-        },
-      },
+      external: ['react-sound', 'react-confetti'], // Exclude react-sound and react-confetti from the bundle
     },
   },
-}); 
+  optimizeDeps: {
+    exclude: ['lucide-react', 'react-sound', 'react-confetti'], // Exclude unnecessary dependencies
+  },
+});
