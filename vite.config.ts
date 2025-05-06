a910a8cd15d0fad767cd8620faba830aaf22df55
Yasir <39470795+Sarcasticoverthinker@users.noreply.github.com>
2025-05-07 03:34:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/sarcasticoverthinker.github.io/',
+  base: '/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name]-[hash][extname]',
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
+      },
+    },
   },
-  optimizeDeps: {
-    exclude: ['lucide-react', 'react-confetti', 'react-sound'],
-  }
 });
